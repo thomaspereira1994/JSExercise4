f9a1f4c826aa3788516a6f62526d4be90cf54c48
thomaspereira94 <thomaspereira1994@outlook.com>
2022-03-18 09:26:58 -0300

    gg

--- js/site.js
+++ js/site.js
@@ -1,4 +1,31 @@
-//call hello world
-function HelloWorld(){
-    alert("hello world");
+function controllerFunction() {
+    let userInput = document.getElementById("userInput");
+    
+    let reversedInput = reverseInput(userInput);
+    let isPalindrome = checkPalindrome(userInput, reversedInput);
+    
+    displayResult(isPalindrome);
+}
+
+function reverseInput(userInput) {
+    let reversedInput = [];
+
+    for (let index = userInput.length - 1; index >= 0; index--) {
+        reversedInput += userInput[index];
+    }
+
+    alert(reversedInput.value);
+    return reversedInput;
+}
+
+function checkPalindrome(userInput, reversedInput) {
+    if (userInput == reversedInput) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function displayResult(isPalindrome) {
+    document.getElementById("resultDisplayArea").value = isPalindrome;
 }
